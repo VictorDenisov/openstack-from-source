5d87b3f075657c9d4fa059a6917c1d085d1f8759
Victor Denisov <denisovenator@gmail.com>
2014-12-09 23:19:27 -0800

    Setup config file

--- manifests/keystone.pp
+++ manifests/keystone.pp
@@ -16,3 +16,25 @@ file { '/etc/hostname':
 package { ['python-dev', 'libxml2-dev', 'libxslt1-dev', 'libsasl2-dev', 'libsqlite3-dev', 'libssl-dev', 'libldap2-dev', 'libffi-dev']:
 	ensure => present,
 }
+
+vcsrepo { "/vagrant/keystone":
+	ensure   => present,
+	provider => git,
+	source   => "https://github.com/openstack/keystone.git",
+	revision => "2014.2",
+	before   => [Package['python-pip'], File['keystone.conf']],
+}
+
+package { 'python-pip':
+	ensure => present,
+	before => Exec['requirements'],
+}
+
+exec { 'requirements':
+	command => '/usr/bin/pip install -r /vagrant/keystone/requirements.txt',
+}
+
+file { 'keystone.conf':
+	path   => '/vagrant/keystone/etc/keystone.conf',
+	source => 'file:///vagrant/keystone/etc/keystone.conf.sample',
+}
