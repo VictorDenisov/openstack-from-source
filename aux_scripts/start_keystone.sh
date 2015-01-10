374cd12e06da1cdbd1f7860b362f3ae8a82dec4a
Victor Denisov <denisovenator@gmail.com>
2015-01-09 19:49:49 -0800

    Add keystone timeout

--- aux_scripts/start_keystone.sh
+++ aux_scripts/start_keystone.sh
@@ -3,5 +3,6 @@
 cd /vagrant/keystone
 ./bin/keystone-manage db_sync
 ./bin/keystone-all &
+sleep 15
 #disown "%/vagrant/keystone/bin/keystone-all"
 /vagrant/aux_scripts/config_keystone.sh 172.15.0.30
